97d33b6a0eb031f07b850c58ba4dfeb5df5c2e23
Jamshid <kalandarovjamshid01@gmail.com>
2022-06-15 17:18:17 +0500

    http methods get post

--- app.js
+++ app.js
@@ -2,16 +2,38 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-const data = JSON.parse(
+app.use(express.json());
+const tours = JSON.parse(
   fs.readFileSync('./dev-data/data/tours-simple.json', 'utf-8')
 );
 
-app.get('/home', (req, res) => {
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: data.length,
-    data: data,
+    results: tours.length,
+    data: tours,
   });
+});
+app.post('/api/v1/tours', (req, res) => {
+  const data = req.body;
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, data);
+  tours.push(newTour);
+  fs.writeFile(
+    './dev-data/data/tours-simple.json',
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        result: tours.length,
+        data: newTour,
+      });
+    }
+  );
+});
+
+app.get('/api/v1/students/:id', (req, res) => {
+  const param = req.params.id;
 });
 
 app.listen(8000, () => {
